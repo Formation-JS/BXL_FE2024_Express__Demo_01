649986afbac26d25272bca82c702491486367b70
Balthazar Picsou <aurelien.strimelle@bstorm.be>
2025-08-19 09:08:19 +0200

    Intro Express 🚆

--- readme.md
+++ readme.md
@@ -1,4 +1,15 @@
 # Demo 01 - Intro Express
 
 ## Installation d'express
-Après avoir initialisé le projet avec node, installation d'express...
+Après avoir initialisé le projet avec node, installation d'express dans le terminal 
+```
+npm i express 
+```
+
+Ensuite, mise en place du serveur web, dans app.js
+```
+//TODO Importer express
+//TODO Initialiser l'app
+//TODO Ajouter une route "hello"
+//TODO Démarrer le serveur
+```
--- src/app.js
+++ src/app.js
@@ -1,1 +1,32 @@
-console.log('Demo 01 - Intro Express');
+//! Importer express
+import express from "express";
+
+//! Constantes
+const PORT = 8080;
+
+//! Initialiser le web server via express
+const app = express();
+
+//! Ajouter une route "hello"
+//? - Route à atteindre (en gros -> l'url)
+//? - Le comportement (Callback avec la requete et la réponse)
+app.get('/coucou', (req, res) => {
+
+    //? Solution basique
+    // res.write('Bonjour');
+    // res.end();
+
+    //? Solution
+    res.send('Bonjour');
+});
+
+//! Démarrer le serveur
+app.listen(PORT, (error) => {
+    if(error) {
+        console.log('Erreur lors du démarage du serveur !');
+        console.log(error);
+        return;
+    }
+
+    console.log(`Le serveur Web tourne sur le port ${PORT}`);
+})
